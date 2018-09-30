219af5319b76dfc47c34b97d601e1030487dda99
Melinda Burke <melccb@gmail.com>
2018-09-30 01:35:13 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,3 +7,54 @@ def display_board(board)
 end
 
 # code your input_to_index and move method here!
+  def input_to_index(user_input)
+    index = user_input.index
+    index -= l
+    return index
+  end
+  
+  # checks user input to see if it is a reall number and if the position is already taken
+  
+  def valid_move?(board, index)
+    def position_taken?(array, ind)
+      if array [ind] == " " || array [ind] == "" || array [ind] == nil
+        return false
+      else
+        return true
+      end
+    end
+    
+  def on_board?(num)
+    if num.between?(0,8) == true
+      return true
+    else
+      return false
+    end
+  end
+  
+    if (position_taken?)(board, index) == false && (on_board?(index) == true)
+      return true
+    else
+      return false
+    end
+  end
+  
+  #Takes user input and places the X or O in that position
+  
+  def move(board, index, character = "X")
+    board[index] = character
+    return board
+  end
+  
+  #Asks user for number, check if valid, ask again if not. 
+  def turn (board)
+    puts "Please enter 1-9:"
+    num = gets.chomp
+    index = input_to_index(num)
+    if valid_move?(board, index) == true
+      move(board, index)
+      display_board(board)
+    else
+      turn(board)
+    end
+  end
